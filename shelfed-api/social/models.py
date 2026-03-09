a052b13626d66cc231800176df5609afd82043cb
Thomas Fleming <sc23tf@leeds.ac.uk>
2026-03-09 20:58:28 +0000

    Improved model validation

--- shelfed-api/social/models.py
+++ shelfed-api/social/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from books.models import Book
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Shelf(models.Model):
@@ -48,7 +49,7 @@ class ReadingLog(models.Model):
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
-    rating = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
