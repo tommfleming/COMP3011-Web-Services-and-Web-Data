f2171cb11c834c1ce80ddfb808bd7c8bbb5eb0ac
Thomas Fleming <sc23tf@leeds.ac.uk>
2026-03-09 20:40:39 +0000

    Django Admin wired and endpoints tested

--- shelfed-api/books/admin.py
+++ shelfed-api/books/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Author, Book
 
-admin.site.register(Author)
-admin.site.register(Book)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "published_year", "genre"]
+    search_fields = ["title", "genre"]
+    filter_horizontal = ["authors"]
--- shelfed-api/social/admin.py
+++ shelfed-api/social/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import Shelf, ShelfItem, ReadingLog, Review, Follow
 
-admin.site.register(Shelf)
-admin.site.register(ShelfItem)
-admin.site.register(ReadingLog)
-admin.site.register(Review)
-admin.site.register(Follow)
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ["name", "owner", "is_public", "created_at"]
+    search_fields = ["name", "owner__username"]
+
+
+@admin.register(ShelfItem)
+class ShelfItemAdmin(admin.ModelAdmin):
+    list_display = ["shelf", "book", "added_at"]
+
+
+@admin.register(ReadingLog)
+class ReadingLogAdmin(admin.ModelAdmin):
+    list_display = ["user", "book", "status", "started_at", "finished_at"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["user", "book", "rating", "created_at"]
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ["follower", "following", "created_at"]
